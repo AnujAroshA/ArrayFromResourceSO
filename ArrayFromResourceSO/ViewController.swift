55fe689779b8084099f15a456882e87837bdd352
AnujAroshA <arosha@creativesoftware.com>
2019-09-02 10:06:36 +0530

    Initial Commit

---
+++ ArrayFromResourceSO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArrayFromResourceSO
+//
+//  Created by Arosha Piyadigama on 9/2/19.
+//  Copyright © 2019 AnujAroshA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
